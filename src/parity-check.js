b400f4005290e6ea566ccdcd03bb15a743264e35
Larisa Romanova <larissa.kstu@gmail.com>
2025-01-13 20:18:19 +0300

    fix brain-even

--- src/parity-check.js
+++ src/parity-check.js
@@ -1,33 +1,46 @@
 import readlineSync from 'readline-sync';
 
-const getGreeting = () => {
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
-
-const getRandomNumber = () => {
-    const max = 100; 
-    return Math.floor(Math.random() * max);
-};
-
-const getAnswer = () => {
-    const answer = readlineSync.question('Your answer: ');
-    if ((randomNumber % 2 === 0 && answer === 'yes') || (randomNumber % 2 !== 0 && answer === 'no')) {
-      console.log(`Correct!`);
-    };
-    console.log(`wrong answer`);
-};
-
-
-
 export default () => {
+  const maxNumber = 100; // обозначаем предел для выборки произвольного числа
+  const roundsNumber = 3; // обозначаем количество раундов
+  let userName = '';  
+  let expectedAnswer; // ожидаемый ответ
+  let receivedAnswer; // полученный ответ
+  
+  // функция для поиска рандомного числа
+  const getRandomNumber = () => { 
+    return Math.floor(Math.random() * maxNumber);
+  };
+
+  // задаем вопрос и определяем какой ответ ожидаем получить
+  const getRound = () => {
+    let randomNumber = getRandomNumber();
+    console.log('Question: ' + randomNumber);
+    if (randomNumber % 2 === 0) {
+      expectedAnswer = 'yes';
+    } else {
+      expectedAnswer = 'no';
+    };
+  };
+
+  // приветсвуем, знакомимся, описываем правила игры
   console.log('Welcome to the Brain Games!');
-  getGreeting();
+  userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-  let randomNumber = getRandomNumber();
-  console.log('Question: ' + randomNumber);
-  getAnswer();
-
-
+  // начинаем игру
+  for (let i = 1; i <= roundsNumber; i += 1) {
+    getRound();
+    receivedAnswer = readlineSync.question('Your answer: ');
+    if (receivedAnswer === expectedAnswer) {
+      console.log(`Correct!`);
+      if (i === 3) {
+        console.log(`Congratulations, ${userName}!`);
+      };
+    } else {
+      console.log(`'${receivedAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`);
+      i = 3; // ответ неверный - заканчиваем игру
+    };
+  };
 };
