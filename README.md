9534c6e9430c2883804235c98e4da28b7c0e8787
Larisa Romanova <larissa.kstu@gmail.com>
2025-01-16 22:07:39 +0300

    fix brain-even

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 [![Actions Status](https://github.com/LarissaRomanova/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/LarissaRomanova/frontend-project-44/actions)
 [![Maintainability](https://api.codeclimate.com/v1/badges/324b317d804573e2619e/maintainability)](https://codeclimate.com/github/LarissaRomanova/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/04YdMN6kInrNJr5vK1Tue9twv.svg)](https://asciinema.org/a/04YdMN6kInrNJr5vK1Tue9twv)
+
+[![asciicast](https://asciinema.org/a/o2lAh54gCEaTrfjE8mITE6Rwb.svg)](https://asciinema.org/a/o2lAh54gCEaTrfjE8mITE6Rwb)
--- src/games/parity-check.js
+++ src/games/parity-check.js
@@ -1,18 +1,13 @@
 import readlineSync from 'readline-sync';
+import { roundsNumber, getGreeting, getRandomNumber, getAnswer } from '../index.js';
 
 export default () => {
   const maxNumber = 100; // обозначаем предел для выборки произвольного числа
-  const roundsNumber = 3; // обозначаем количество раундов
-  let userName = '';
   let expectedAnswer; // ожидаемый ответ
-  let receivedAnswer; // полученный ответ
-  // поиск рандомного числа
-  const getRandomNumber = () => {
-    return Math.floor(Math.random() * maxNumber);
-  };
+
   // задаем вопрос и определяем какой ответ ожидаем получить
   const getRound = () => {
-    const randomNumber = getRandomNumber();
+    const randomNumber = getRandomNumber(maxNumber);
     console.log('Question: ' + randomNumber);
     if (randomNumber % 2 === 0) {
       expectedAnswer = 'yes';
@@ -22,23 +17,11 @@ export default () => {
   };
 
   // приветсвуем, знакомимся, описываем правила игры
-  console.log('Welcome to the Brain Games!');
-  userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  getGreeting();
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-
-  // начинаем игру
   for (let i = 1; i <= roundsNumber; i += 1) {
     getRound();
-    receivedAnswer = readlineSync.question('Your answer: ');
-    if (receivedAnswer === expectedAnswer) {
-      console.log('Correct!');
-      if (i === roundsNumber) {
-        console.log(`Congratulations, ${userName}!`); // дано предельное количество верных ответов заканчиваем игру
-      }
-    } else {
-      console.log(`'${receivedAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer}'.\nLet's try again, ${userName}!`);
-      i = roundsNumber; // ответ неверный - заканчиваем игру
-    }
+    const isCorrectAnswer = getAnswer(expectedAnswer, i);
+    if (!isCorrectAnswer) return;
   }
 };
